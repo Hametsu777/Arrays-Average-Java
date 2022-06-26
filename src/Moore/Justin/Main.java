fa25e8f64f638098ce804a5ec411a384e04ff7b9
Hametsu777 <Hametsu777@gmail.com>
2022-06-26 17:20:46 -0400

    Remove commented code

--- src/Moore/Justin/Main.java
+++ src/Moore/Justin/Main.java
@@ -12,23 +12,6 @@ public class Main {
             System.out.println("Element " + i + ", typed value was " + myIntegers[i]);
         }
         System.out.println("The average is " + getAverage(myIntegers));
-       /* int[] myIntArray = new int[25];//{1,2,3,4,5,6,7,8,9,10};
-        //myIntArray[5] = 50;
-        double[] myDoubleArray = new double[10];
-        System.out.println(myIntArray[5]);
-        System.out.println(myIntArray[0]);
-        System.out.println(myIntArray[6]);
-        System.out.println(myIntArray[8]);
-        for(int i = 0; i < myIntArray.length; i++)
-        {
-            myIntArray[i] = i * 10;
-        }
-
-        printArray(myIntArray);
-        *//*for (int i = 0; i < myIntArray.length; i++)
-        {
-            System.out.println("Element " + i + ", value is " + myIntArray[i]);
-        }*/
     }
 
     public static void printArray(int[] array)
